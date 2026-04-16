3cd5e10998cfd96117ca004896d29cf4713096f4
stuff on junk server <stuff@junk.sudoroom.org>
2026-04-16 12:59:15 -0700

    fix systemd logfile settings, attempt auto-reload of html files

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/stuff/stuff-org/stuff
-./stuff -imagedir img-srv/
+#         Cache templates. False for online editing while development. (default true)
+#         unfortunately it doesn't seem to work.  We still have to restart to reload html files
+./stuff -cache-templates false -imagedir img-srv/
